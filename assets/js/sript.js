7ae2e8e29aaea86ac5c5f30b809894792b7234f4
Michael Leese <illumni@outlook.com>
2019-02-09 15:13:02 +0000

    functions

---
+++ assets/js/sript.js
@@ -0,0 +1,59 @@
+function poppyFn(){
+  
+  document.querySelector('#poppy-pic').style.display = "block";
+
+ var closePop = setInterval(closePoppy, 2500)
+ 
+ function closePoppy() {
+   document.querySelector('#poppy-pic').style.display = "none";
+   clearInterval(closePop);
+ }
+}
+
+function redFn(){
+  
+  document.querySelector('#red-pic').style.display = "block";
+
+ var closePop = setInterval(closePoppy, 2500)
+ 
+ function closePoppy() {
+   document.querySelector('#red-pic').style.display = "none";
+   clearInterval(closePop);
+ }
+}
+
+function yellowFn(){
+  
+  document.querySelector('#yellow-pic').style.display = "block";
+
+ var closePop = setInterval(closePoppy, 2500)
+ 
+ function closePoppy() {
+   document.querySelector('#yellow-pic').style.display = "none";
+   clearInterval(closePop);
+ }
+}
+
+function blueFn(){
+  
+  document.querySelector('#blue-pic').style.display = "block";
+
+ var closePop = setInterval(closePoppy, 2500)
+ 
+ function closePoppy() {
+   document.querySelector('#blue-pic').style.display = "none";
+   clearInterval(closePop);
+ }
+}
+
+function pinkFn(){
+  
+  document.querySelector('#pink-pic').style.display = "block";
+
+ var closePop = setInterval(closePoppy, 2500)
+ 
+ function closePoppy() {
+   document.querySelector('#pink-pic').style.display = "none";
+   clearInterval(closePop);
+ }
+}
