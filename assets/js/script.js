a73b465834b34bcb733162573a2af58ebebbec41
Michael Leese <illumni@outlook.com>
2019-02-09 15:34:02 +0000

    change spelling mistake in script foldername and adjust styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,58 +2,63 @@ function poppyFn(){
   
   document.querySelector('#poppy-pic').style.display = "block";
 
- var closePop = setInterval(closePoppy, 2500)
+ var closePop = setInterval(closePoppy, 2500);
  
  function closePoppy() {
    document.querySelector('#poppy-pic').style.display = "none";
    clearInterval(closePop);
- };
-};
+ }
+ console.log('poppy');
+}
 
 function redFn(){
   
   document.querySelector('#red-pic').style.display = "block";
 
- var closePop = setInterval(closePoppy, 2500)
+ var closePop = setInterval(closePoppy, 2500);
  
  function closePoppy() {
    document.querySelector('#red-pic').style.display = "none";
    clearInterval(closePop);
- };
-};
+ }
+ console.log('red');
+}
 
 function yellowFn(){
   
   document.querySelector('#yellow-pic').style.display = "block";
 
- var closePop = setInterval(closePoppy, 2500)
+ var closePop = setInterval(closePoppy, 2500);
  
  function closePoppy() {
    document.querySelector('#yellow-pic').style.display = "none";
    clearInterval(closePop);
- };
-};
+ }
+ console.log('yellow');
+}
 
 function blueFn(){
   
   document.querySelector('#blue-pic').style.display = "block";
 
- var closePop = setInterval(closePoppy, 2500)
+ var closePop = setInterval(closePoppy, 2500);
  
  function closePoppy() {
    document.querySelector('#blue-pic').style.display = "none";
    clearInterval(closePop);
- };
-};
+ }
+ console.log('blue');
+}
 
 function pinkFn(){
   
   document.querySelector('#pink-pic').style.display = "block";
 
- var closePop = setInterval(closePoppy, 2500)
+ var closePop = setInterval(closePoppy, 2500);
  
  function closePoppy() {
    document.querySelector('#pink-pic').style.display = "none";
    clearInterval(closePop);
- };
-};
+ }
+ console.log('pink');
+}
